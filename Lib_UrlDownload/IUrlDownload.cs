e87fba967fa51298a111eb89ddc7d0c86a15c38d
Eduardo Mariano Tonaco <111458446+EduardoMTonaco@users.noreply.github.com>
2022-11-17 15:27:20 -0300

    Add Summary on the Interface

--- Lib_UrlDownload/IUrlDownload.cs
+++ Lib_UrlDownload/IUrlDownload.cs
@@ -3,11 +3,26 @@ using System.Threading.Tasks;
 
 namespace Lib_UrlDownload
 {
+    /// <summary>
+    /// Interface to return the site http in string
+    /// </summary>
     public interface IUrlDownload
     {
+        /// <summary>
+        /// The url of the site can be set in construtc
+        /// </summary>
         string Url { get; }
-
+        /// <summary>
+        /// method to return as a string the html of a site seted in the constructor
+        /// </summary>
+        /// <param name="ct">Cancelation Token</param>
+        /// <returns>HTML as string</returns>
         Task<string> SiteToString();
+        /// <summary>
+        /// method to return as a string the html of a site seted in the constructor, with cancellationToken
+        /// </summary>
+        /// <param name="ct">Cancelation Token</param>
+        /// <returns>HTML as string</returns>
         Task<string> SiteToString(CancellationToken ct);
     }
 }
